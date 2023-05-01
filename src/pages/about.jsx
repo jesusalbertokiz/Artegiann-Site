fe61cb1b846a85f1a94eb119f39fbc3ff7f8c1df
Jesus Alberto Mora Lozada <88741710+jesusalbertokiz@users.noreply.github.com>
2023-05-01 21:57:09 +0000

    Modifique el contenido de about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,32 +14,52 @@ const AboutPage = () => {
         <div sx={{ marginTop: 4, marginBottom: 4 }}>
           <Grid container spacing={4}>
             <Grid item xs={12} sx={{ width: "100%", sm: { width: "50%" }, md: { width: "33.33%" }}}>
-              <Typography variant="h4" gutterBottom>
+              <Typography variant="h1" color="secondary" gutterBottom>
                 ¡Hola! Mi nombre es Giannella A. Casilli T.
               </Typography>
-              <Typography variant="body1" paragraph>
-                Soy una desarrolladora de software con experiencia en JavaScript,
-                React, Node.js, y otras tecnologías web. Me apasiona crear
-                soluciones innovadoras y escalables para problemas reales.
+              <Typography variant="body1" color="white" paragraph>
+                Desde que era pequeña, siempre he tenido una gran pasión por el mundo de
+                las artes, desarrollando habilidades en la pintura, el dibujo, la
+                escultura en plastilina, el diseño y la escritura de cuentos.
+              </Typography>
+              <Typography variant="body1" color="white" paragraph>
+                Además, soy una emprendedora y creadora de mi propia marca de diseños
+                exclusivos llamada ArteGiann, y también me dedico a la creación digital
+                de NFTs, que en un mes aproximadamente estarán en el mercado.
               </Typography>
             </Grid>
             <Grid item xs={12} sx={{ width: "100%", sm: { width: "50%" }, md: { width: "33.33%" }}}>
-              <Typography variant="h4" gutterBottom>
-                ¿Qué puedo hacer por ti?
+              <Typography variant="h2" color="secondary" gutterBottom>
+                Mis pasiones
               </Typography>
-              <Typography variant="body1" paragraph>
-                Si necesitas ayuda con tu proyecto, no dudes en contactarme. Estoy
-                disponible para trabajar en proyectos freelance y también para
-                colaborar en proyectos de código abierto.
+              <Typography variant="body1" color="white" paragraph>
+                Soy una apasionada del Anime (series japonesas), la naturaleza, defensora
+                del medio ambiente y los animales. Me siento muy identificada con las
+                energías, la espiritualidad y el universo.
+              </Typography>
+              <Typography variant="body1" color="white" paragraph>
+                A pesar de mi corta edad y de estar dentro del Espectro Autista, siempre estoy rompiendo paradigmas y
+                desarrollándome en cada una de las metas que me propongo. Siempre quiero
+                dar un paso más allá y sé que mi destino está en mis propias manos. Soy
+                una firme creyente de que si se tiene pasión por lo que se ama, se pueden
+                alcanzar todos los sueños.
               </Typography>
             </Grid>
             <Grid item xs={12} sx={{ width: "100%", sm: { width: "50%" }, md: { width: "33.33%" }}}>
-              <Typography variant="h4" gutterBottom>
-                ¿Quieres saber más?
+              <Typography variant="h2" color="secondary" gutterBottom>
+                Formación académica
               </Typography>
-              <Typography variant="body1" paragraph>
-                Puedes encontrar más información sobre mi experiencia y proyectos
-                en mi sitio web y en mi perfil de LinkedIn.
+              <Typography variant="body1" color="white" paragraph>
+              En cuanto a mi formación académica, he dedicado mucho esfuerzo y
+              dedicación para alcanzar mis objetivos. He culminado el High School
+              &#40;bachillerato internacional&#41;  en el Dawere International High School, y
+              el bachillerato en Valencia, Venezuela, con la mención honorífica SUMMA
+              CUM LAUDE. Actualmente, estoy becada por la Escuela de Escritores de la
+              periodista y escritora Venezolana NACARID PORTAL, autora de mi primer
+              cuento “Una relevante conversación”. Además, estoy iniciando un Diplomado
+              de Gestión Ambiental en la UNESCO. También culminé el curso de inglés en
+              la Universidad de Carabobo, Laboratorio de Idiomas, en Valencia –
+              Venezuela
               </Typography>
             </Grid>
           </Grid>
