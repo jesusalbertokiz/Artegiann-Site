10dc9acd9b23877e41bd37df7f466ebbd9a5d731
Jesus Alberto Mora Lozada <88741710+jesusalbertokiz@users.noreply.github.com>
2023-05-01 16:44:16 +0000

    Se agrego la pagina de contacto

---
+++ src/pages/contact.jsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import theme from "../theme"
+import { ThemeProvider } from "@mui/material"
+import Layout from "../components/layout";
+import Hero from "../components/hero";
+import DiscoverSection from "../components/discoverSection";
+import UnifreeHistory from "../components/unifreeHistory";
+import AboutSection from "../components/aboutSection";
+
+
+const IndexPage = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <p>Esta será la pagina de contacto</p>
+      </Layout>
+    </ThemeProvider>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
