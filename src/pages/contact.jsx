e5946483660857a5d3d7134b4fca985db05d7142
Jesus Alberto Mora Lozada <88741710+jesusalbertokiz@users.noreply.github.com>
2023-05-02 05:26:30 +0000

    Aguregue la maquetacion de la pagina de contacto

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,17 +2,38 @@ import * as React from "react"
 import theme from "../theme"
 import { ThemeProvider } from "@mui/material"
 import Layout from "../components/layout";
-import Hero from "../components/hero";
-import DiscoverSection from "../components/discoverSection";
-import UnifreeHistory from "../components/unifreeHistory";
-import AboutSection from "../components/aboutSection";
+import { Button, Container, Grid, IconButton, Typography } from '@mui/material';
+import TiktokIcon from './tiktokIcon'
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 
 const ContactPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        <p>Esta será la pagina de contacto</p>
+        <Container>
+          <Grid container spacing={2} justifyContent="center" alignItems="center">
+            <Grid item xs={12} textAlign="center">
+              <Typography variant="h3" component="h1" gutterBottom>Contacto</Typography>
+            </Grid>
+            <Grid item xs={12} textAlign="center">
+              <Typography variant="body1" gutterBottom>¡Síguenos en nuestras redes sociales!</Typography>
+            </Grid>
+            <Grid item xs={12} textAlign="center">
+              <IconButton href="https://www.instagram.com/" target="_blank" rel="noopener">
+                <InstagramIcon />
+              </IconButton>
+              <IconButton href="https://www.tiktok.com/" target="_blank" rel="noopener">
+                <TiktokIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={12} textAlign="center">
+              <Button variant="contained" color="primary" href="/">
+                Volver al inicio
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
       </Layout>
     </ThemeProvider>
   )
@@ -21,3 +42,4 @@ const ContactPage = () => {
 export default ContactPage
 
 export const Head = () => <title>Contacto</title>
+
