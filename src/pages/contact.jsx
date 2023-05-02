01f71fc04ac25492e5618f9fa53b84daf432ce88
Jesus Alberto Mora Lozada <88741710+jesusalbertokiz@users.noreply.github.com>
2023-05-02 06:04:27 +0000

    Agregue tres contenedores que sustituyen los iconos que estaban debajo del texto

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,6 +7,9 @@ import TiktokIcon from '../components/tiktokIcon'
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 
+import { Grid, IconButton, Typography, Button } from "@mui/material";
+import { Instagram, Tiktok, WhatsApp } from "@mui/icons-material";
+
 const ContactPage = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -17,19 +20,40 @@ const ContactPage = () => {
               <Typography variant="h1" color="secondary" gutterBottom>Contacto</Typography>
             </Grid>
             <Grid item xs={12} textAlign="center">
-              <Typography variant="body1" gutterBottom>
-              ¡Hola! Si te gustaría contactar con Giannella, la creadora de ArteGiann, para conocer 
-              más sobre sus diseños exclusivos y creatividad, o para adquirir algún producto en su 
-              tienda en línea, aquí te dejamos sus medios de contacto
+              <Typography variant="body1" color="primary" gutterBottom>
+                ¡Hola! Si te gustaría contactar con Giannella, la creadora de ArteGiann, para conocer más sobre sus diseños exclusivos y creatividad, o para adquirir algún producto en su tienda en línea, aquí te dejamos sus medios de contacto
               </Typography>
             </Grid>
             <Grid item xs={12} textAlign="center">
-              <IconButton href="https://www.instagram.com/" target="_blank" rel="noopener">
-                <InstagramIcon color="#7b1fa2" />
-              </IconButton>
-              <IconButton href="https://www.tiktok.com/" target="_blank" rel="noopener">
-                <TiktokIcon />
-              </IconButton>
+              <Grid container spacing={2} justifyContent="center" alignItems="center" style={{margin:'auto'}}>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="body1" color="primary" gutterBottom>
+                    Redes sociales: Puedes seguir a Giannella en Twitter, Instagram y Facebook, donde comparte sus obras de arte y su creatividad. Encuéntrala en @artegiannct.
+                  </Typography>
+                  <Grid item xs={12} textAlign="center">
+                    <IconButton href="https://www.instagram.com/" target="_blank" rel="noopener">
+                      <InstagramIcon style={{color:'#7b1fa2'}} />
+                    </IconButton>
+                    <IconButton href="https://www.tiktok.com/" target="_blank" rel="noopener">
+                      <TiktokIcon />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="body1" color="primary" gutterBottom>
+                    WhatsApp: También puedes escribirle vía WhatsApp al número +58 414-439.64.02 para hacerle preguntas o hacerle pedidos.
+                  </Typography>
+                  <WhatsApp />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="body1" color="primary" gutterBottom>
+                    Tiendas en línea: Si quieres adquirir algún producto de ArteGiann, puedes visitar sus tiendas en línea en artegiann.atienda.la.
+                  </Typography>
+                  <Link href="https://www.artegiann.atienda.la" target="_blank" rel="noopener">
+                    <ShoppingCart />
+                  </Link>
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item xs={12} textAlign="center">
               <Button variant="contained" color="primary" href="/">
